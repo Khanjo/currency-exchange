32ca797edeef2736db0770b1813609f6a0640a90
Joshua Khan <Khanjo@gmail.com>
2023-07-10 07:41:59 -0700

    fix the API call

--- src/currencyAPI.js
+++ src/currencyAPI.js
@@ -1,7 +1,7 @@
 export class CurrencyExchange {
     static async getExchange(code1) {
         try {
-            const response = await fetch(`https://v6.xchangerate-api.com/v6/${process.env.API_KEY}/latest/${code1}`);
+            const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${code1}`);
             const jsonResponse = await response.json();
             if (!response.ok) {
                 const errorMessage = `sorry, your request could not be processed ${response.status} ${response.statusText}`;
